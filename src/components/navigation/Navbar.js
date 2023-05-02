8c65de5ab797b14ac1941969ab782b4a875411d6
Steve Pflug <stephen@pflug.us>
2023-05-02 15:40:17 -0500

    added some styling for navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -17,6 +17,14 @@ const NavbarWrapper = styled.nav`
   width:100%;
   height: 60px;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   position: sticky;
+  padding: 1rem;
+  background-color: darkslategrey;
+  color: white;
+  
+  a:visited {
+    color: white;
+  }
 `;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {createRoot} from "react-dom/client";
 import App from "./App";
 import {NavigationProvider} from "./context/navigation";
+import "./index.css";
 
 
 const el = document.getElementById('root');
