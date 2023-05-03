ca4f48fe97efc613adb63920f3e4362e4c29ccb8
Steve Pflug <stephen@pflug.us>
2023-05-03 11:20:44 -0500

    authentication working with JWT

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -13,6 +13,29 @@ function LoginForm() {
 
     const handleSubmit = async e => {
         e.preventDefault();
+        // const bodyObject = JSON.stringify({
+        //     username: inputs.username,
+        //     password: inputs.password,
+        // });
+        // let formData = new FormData();
+        // formData.append('username', inputs.username);
+        // formData.append('password', inputs.password);
+        // console.log(bodyObject);
+        // const results = await fetch("http://localhost:8080/login", {
+        //     method: 'POST',
+        //     // headers: {
+        //     //     'Content-Type': 'application/x-www-form-urlencoded',
+        //     // },
+        //     body: formData
+        // });
+        // console.log(results);
+        // if(results.ok) {
+        //     console.log("Ok Response! Attempting to get token");
+        //     const tokenResults = await fetch("http://localhost:8080/token", {
+        //         method: 'POST',
+        //     });
+        //     console.log(tokenResults);
+        // }
         const results = await fetch("http://localhost:8080/login", {
             method: 'POST',
             headers: {
@@ -24,16 +47,20 @@ function LoginForm() {
             })
         });
         console.log(results);
-        // const results = await fetch("http://localhost:8080/signin", {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //         username: inputs.username,
-        //         password: inputs.password,
-        //     })
-        // });
+        if(results.ok) {
+            const data = await results.text();
+            console.log(data);
+            const getResults = await fetch("http://localhost:8080/api/groups", {
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${data}`,
+                },
+            });
+            console.log(getResults);
+            const getData = await getResults.json();
+            console.log(getData);
+        }
+
         // if(results.ok) {
         //     console.log("Ok response! trying to get token");
         //     const tokenResults = await fetch("http://localhost:8080/token", {
