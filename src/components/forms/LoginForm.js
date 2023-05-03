8a4c0faebf9df2f3d6334ea53708e6fe245706fe
Steve Pflug <stephen@pflug.us>
2023-05-03 09:34:01 -0500

    testing new fetch request

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,10 +1,69 @@
 import styled from "styled-components";
+import {useState} from "react";
 
 
 function LoginForm() {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    };
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const results = await fetch("http://localhost:8080/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: inputs.username,
+                password: inputs.password,
+            })
+        });
+        console.log(results);
+        // const results = await fetch("http://localhost:8080/signin", {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({
+        //         username: inputs.username,
+        //         password: inputs.password,
+        //     })
+        // });
+        // if(results.ok) {
+        //     console.log("Ok response! trying to get token");
+        //     const tokenResults = await fetch("http://localhost:8080/token", {
+        //         method: 'POST',
+        //     });
+        //     console.log(tokenResults);
+        // } else {
+        //     console.log("Bad response!");
+        // }
+
+    };
+
     return(
-        <LoginFormWrapper>
-            Login Form
+        <LoginFormWrapper onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <input
+                type={"text"}
+                name={"username"}
+                placeholder={"Username"}
+                value={inputs.username || ""}
+                onChange={handleChange}
+            />
+            <input
+                type={"password"}
+                name={"password"}
+                placeholder={"Password"}
+                value={inputs.password || ""}
+                onChange={handleChange}
+            />
+            <button type={"submit"}>Submit</button>
         </LoginFormWrapper>
     );
 }
