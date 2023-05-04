28d8b30240ab9a64c68229e4f2161426c54ab9c3
Steve Pflug <stephen@pflug.us>
2023-05-04 16:42:48 -0500

    fixed bug with react app not compiling

--- src/pages/BudgetPage.js
+++ src/pages/BudgetPage.js
@@ -1,15 +1,32 @@
 import styled from "styled-components";
+import {PageWrapper} from "../styles/PageWrapper.styled";
+import {useEffect} from "react";
+import {useSelector} from "react-redux";
 
 function BudgetPage() {
+    const {token} = useSelector(state => state.jwt);
+
+    useEffect(() => {
+        const createLinkToken = async () => {
+            const results = await fetch("http://localhost:8080/api/plaid/create-link-token", {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+            console.log(results);
+            const data = await results.text();
+            console.log(data);
+        }
+        if(token) {
+            createLinkToken();
+        }
+    }, []);
     return(
-        <BudgetPageWrapper>
+        <PageWrapper>
             This should only show if authenticated
-        </BudgetPageWrapper>
+        </PageWrapper>
     );
 }
 
 export default BudgetPage;
-
-const BudgetPageWrapper = styled.div`
-  
-`;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,26 @@
 import styled from "styled-components";
 import {PageWrapper} from "../styles/PageWrapper.styled";
 import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 
 function HomePage() {
     const {token} = useSelector(state => state.jwt);
+
+    useEffect(() => {
+        const getGroups = async () => {
+            const results = await fetch("http://localhost:8080/api/groups", {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+            console.log(results);
+        };
+        if(token) {
+            getGroups();
+        }
+    });
 
     return(
         <PageWrapper>
