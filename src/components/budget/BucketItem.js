4b4c0035f67e501acc461039d93cf3c014377f92
Steve Pflug <stephen@pflug.us>
2023-05-10 10:21:31 -0500

    adding bucketlist and bucketitem components

---
+++ src/components/budget/BucketItem.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+function BucketItem() {
+    return(
+        <BucketItemWrapper>
+            Bucket Item
+        </BucketItemWrapper>
+    );
+}
+
+export default BucketItem;
+
+const BucketItemWrapper = styled.div`
+  
+`;
---
+++ src/components/budget/BucketList.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+function BucketList() {
+    return(
+        <BucketListWrapper>
+            Bucket List
+        </BucketListWrapper>
+    );
+}
+
+export default BucketList;
+
+const BucketListWrapper = styled.div`
+  
+`;
