33f758e4e5427d36dc3820d7923b4822611d91b1
Steve Pflug <stephen@pflug.us>
2023-05-10 10:37:46 -0500

    building out first endpoint to test

--- src/store/apis/budgetApi.js
+++ src/store/apis/budgetApi.js
@@ -1,16 +1,33 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 
+
+
 const budgetApi = createApi({
     reducerPath: 'budget',
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:8080/api',
+        prepareHeaders: (headers, { getState }) => {
+            const jwtToken = getState().keys.jwtToken;
+            if(jwtToken) {
+                headers.set('Authorization', `Bearer ${jwtToken}`);
+                return headers;
+            }
+        },
     }),
     endpoints(builder) {
         return {
             fetchBudget: builder.query({
-
+                query() {
+                    return {
+                        url: "/groups",
+                    };
+                }
             })
         }
     }
 });
+
+export const {
+    getFetchBudgetQuery
+} = budgetApi;
