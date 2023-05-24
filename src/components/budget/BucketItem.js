5e8a457cf6d5502cfdabeabce3b5a62c4b3ab921
Stephen Pflug <stephen@pflug.us>
2023-05-23 20:45:01 -0500

    added some logic in BucketItem component to show delete and edit buttons. mores styling and functionality needed

--- src/components/budget/BucketItem.js
+++ src/components/budget/BucketItem.js
@@ -1,9 +1,36 @@
 import styled from "styled-components";
+import Button from "../simple/Button";
+import {GoPencil, GoTrashcan} from "react-icons/go";
+import {useDeleteBucketMutation, useEditBucketMutation} from "../../store";
 
 
 function BucketItem({bucket}) {
+    const [editBucket, editResults] = useEditBucketMutation();
+    const [deleteBucket, deleteResults] = useDeleteBucketMutation();
+
+    const handleDeleteBucket = () => {
+        console.log("Inside handleDeleteBucket");
+    };
+
+    const handleEditBucketClick = () => {
+        console.log("Inside handleEditBucket");
+    };
+
     return(
         <BucketItemWrapper>
+            <BucketHeader>
+                <div>
+                    {bucket.name}
+                </div>
+                <IconWrapper>
+                    <Button danger onClick={handleDeleteBucket} loading={deleteResults.isLoading}>
+                        <GoTrashcan />
+                    </Button>
+                    <Button secondary onClick={handleEditBucketClick} loading={editResults.isLoading}>
+                        <GoPencil />
+                    </Button>
+                </IconWrapper>
+            </BucketHeader>
             {bucket.name}
             {bucket.recurringAmount}
             {bucket.maximumAmount}
@@ -15,4 +42,15 @@ export default BucketItem;
 
 const BucketItemWrapper = styled.div`
   
+`;
+
+const BucketHeader = styled.div``;
+
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  & button {
+    margin: 0 0.25rem;
+  }
 `;
