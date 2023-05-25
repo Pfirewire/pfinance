edf8ce38535510244127409be8937f8d93cb6afb
Steve Pflug <stephen@pflug.us>
2023-05-25 08:20:35 -0500

    added SignupForm component

---
+++ src/components/forms/SignupForm.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+function SignupForm() {
+    return(
+        <SignupFormWrapper>
+            Signup Form
+        </SignupFormWrapper>
+    );
+}
+
+export default SignupForm;
+
+const SignupFormWrapper = styled.div``;
