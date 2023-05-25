48cc337d81c258f74d125153daf31fc36ff4c2e6
Steve Pflug <stephen@pflug.us>
2023-05-25 10:40:50 -0500

    changed LoginPage component to show either login or register form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,42 @@
 import styled from "styled-components";
 import LoginForm from "../components/forms/LoginForm";
 import {PageWrapper} from "../styles/PageWrapper.styled";
+import {useState} from "react";
+import RegisterForm from "../components/forms/RegisterForm";
+import Button from "../components/simple/Button";
 
 
 function LoginPage() {
+    const [showRegisterForm, setShowRegisterForm] = useState(false);
+
+    const handleShowRegisterForm = () => {
+        setShowRegisterForm(true);
+    };
+
+    const handleShowLoginForm = () => {
+        setShowRegisterForm(false);
+    };
+
+    let content;
+    if(showRegisterForm) {
+        content = (
+            <>
+                <RegisterForm />
+                <Button onClick={handleShowLoginForm}>Show Login Form</Button>
+            </>
+        );
+    } else {
+        content = (
+            <>
+                <LoginForm />
+                <Button onClick={handleShowRegisterForm}>Show Register Form</Button>
+            </>
+        );
+    }
+
     return(
         <PageWrapper>
-            <LoginForm />
+            {content}
         </PageWrapper>
     );
 }
