17ee1cb00c74bb58e8463d2ab56f4da492b54dc7
Steve Pflug <stephen@pflug.us>
2023-06-05 12:01:53 -0500

    created skeleton for Budget component

---
+++ src/components/budget/Budget.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+function Budget() {
+    return(
+        <BudgetWrapper>
+            Budget
+        </BudgetWrapper>
+    );
+}
+
+export default Budget;
+
+const BudgetWrapper = styled.div`
+  
+`;
